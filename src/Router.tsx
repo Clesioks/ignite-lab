8c152d752f225913561ab0ef8a6293cf9fc271b5
Clesioks <kornalewski@outlook.com>
2022-07-17 20:30:12 -0300

    routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Event from './Pages/Event';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/event" element={<h1>Event</h1>} />
+      <Route path="/event" element={<Event />} />
     </Routes>
   );
 }
