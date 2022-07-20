07fecae953e83a110f42c7747f16a17b60d468d0
Clesioks <kornalewski@outlook.com>
2022-07-20 19:25:55 -0300

    background

---
+++ src/Pages/Subscribe.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Subscribe() {
+  return (
+    <div
+      className="min-h-screen bg-blur 
+    bg-cover bg-no-repeat flex flex-col 
+    items-center"
+    ></div>
+  );
+}
